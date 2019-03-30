20b2defb5ea90c5c54d127c5255b5934b4d949b2
Bao Nguyen <bao.ng.nguyen3@gmail.com>
2019-03-30 14:44:29 -0400

    working on Nav

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React,{Component}from 'react';
+import Nav from './Nav';
+
+class App extends Component{
+  render(){
+    <div>
+      <h1>Acme Users With Ranks</h1>
+      <Nav />
+    </div>
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 
 const root = document.querySelector('#root');
-ReactDOM.render(<hr />, root);
+ReactDOM.render(<App />, root);
 
 
