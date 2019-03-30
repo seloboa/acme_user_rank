2c72a6e5c2c93204779c0b9b206df3afa91f851e
Bao Nguyen <bao.ng.nguyen3@gmail.com>
2019-03-30 14:56:40 -0400

    finish Nav

--- src/Nav.js
+++ src/Nav.js
@@ -1,34 +1,37 @@
 import React, {Fragment} from 'react';
 import {NavLink, Route} from 'react-router-dom';
+import Users from './Users';
+import UserForm from './UserForm'
 
 const Nav = () => {
   return (
     <Fragment>
       <ul className="nav nav-tabs">
-      <li className="nav-item">
-        <NavLink exact to={'/'} className={'nav-link'}>
-          Home
-        </NavLink>
-      </li>
-      <li className="nav-item">
-        <NavLink exact to={'/users'} className={'nav-link'}>
-          Users
-        </NavLink>
-      </li>
-      <li className="nav-item">
-        <NavLink to={'/users/create'} className={'nav-link'}>
-          Create A User
-        </NavLink>
-      </li>
-      <li className="nav-item">
-        <NavLink to={'/users/topRanked'} className={'nav-link'}>
-          Top Ranked
-        </NavLink>
-      </li>
-    </ul>
-    <Route exact path={'/'} render={()=><p>We have x users!</p>}></Route>
+        <li className="nav-item">
+          <NavLink exact to={'/'} className={'nav-link'}>
+            Home
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink exact to={'/users'} className={'nav-link'}>
+            Users
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink to={'/users/create'} className={'nav-link'}>
+            Create A User
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink to={'/users/topRanked'} className={'nav-link'}>
+            Top Ranked
+          </NavLink>
+        </li>
+      </ul>
+      <Route exact path={'/'} render={() => <p>We have x users!</p>} />
+      <Route exact path={'/users'} component={Users} />
+      <Route path ={'/users/create'} component={UserForm}></Route>
     </Fragment>
-
   );
 };
 
